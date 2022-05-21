466d44ec2b68148c53b71de87fc0d5230e9cda69
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2022-05-21 19:02:47 +0530

    Create Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {DataProvider} from './GlobalState'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import Header from './Components/Home/Header';
+import Home from './Components/Home/Home';
+import Services from './Components/Home/Service';
+import About from './Components/Home/About';
+import Contact from './Components/Home/Contact';
+import Error from './Components/Home/Error';
 
 function App() {
-    return (
-      <DataProvider>
-        <Router>
-          <div>
-            
-          </div>
-        </Router>
-      </DataProvider>
-    );
-  }
-  
-  export default App;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/service" element={<Services />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="*" element={<Error />} />
+      </Routes>
+    </>
+
+
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Home/Header.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Header() {
+    return (
+
+        <div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <nav class="navbar bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#">
+                                <img src="https://www.sliit.lk/wp-content/uploads/2017/09/SLIIT.png" alt="SLIIT" width="120" height="50" class="d-inline-block align-text-top" />
+                            </a>
+                        </div>
+                    </nav>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <a class="navbar-brand text-danger" href="/">Project Management Tool </a>
+                            <li class="nav-item">
+                                <a class="nav-link active text-dark" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="/service">Service</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="/about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="/contact">Contact</a>
+                            </li>
+                        </ul>
+                        <li>
+                            <NavLink className="btn btn-outline-primary ms-2 px-4 rounded-pill" to="/login">
+                                Login <i className="fa fa-sign-in me-2" />
+                            </NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/register"> <button className="btn btn-outline-primary ms-2 px-4 rounded-pill">
+                                Register <i className="fa fa-user-plus me-2" /> </button>
+                            </NavLink>
+                        </li>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    )
+}
+
+export default Header;
